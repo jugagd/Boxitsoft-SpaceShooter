f357dabd78d8b68e0a3bfd52b6262f934113a4a9
Juan Garcia <jugagd@gmail.com>
2019-02-03 12:35:34 -0300

    'backup'

---
+++ SpaceShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    int highscore;
+    int actualScore;
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameManager");
+        if (objs.Length > 1)        
+            Destroy(this.gameObject);        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Highscore"))
+            highscore = PlayerPrefs.GetInt("Highscore");
+    }
+
+    void Update () {
+        if (actualScore>highscore)
+        {
+            highscore = actualScore;
+            PlayerPrefs.SetInt("Highscore",highscore);
+        }		
+	}
+}
